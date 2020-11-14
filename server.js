2a7babe8923ba0fa6f62765103a2df246da62c26
arukuskus <74439490+arukuskus@users.noreply.github.com>
2020-11-14 09:41:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-
 let http = require('http');
 var fs = require('fs');
-const server = http.createServer(function(req, res) => {
-    console.log('URL страницы: ${req.url}');
-    if(req.url === '/'){
-        sendRes('comment.html', 'text/html', res);
-    }
-
+let server = http.createServer(function(req, res) {
+console.log("URL страницы: " + req.url);
+res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+let myReadShort = fs.createReadStream(__dirname + '/comment.html', 'utf8');
+//res.end('Привет, мир!');
+myReadShort.pipe(res);
 });
-
 
 server.listen(3000, '192.168.0.102');
 console.log("Мв отслеживаем порт 3000");
