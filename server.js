2eaa38fd6365183c7f6a837fb90df37f2421c155
Petr Matyukov <kuruhuru@gmail.com>
2020-11-16 12:38:55 +0700

    Added server1

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ let http = require('http');
 var fs = require('fs');
 let server = http.createServer(function(req, res) {
 console.log("URL страницы: " + req.url);
-res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+res.setHeader("Content-Type", "text/html");
+res.writeHead(200);
 let myReadShort = fs.createReadStream(__dirname + '/comment.html', 'utf8');
-//res.end('Привет, мир!');
-myReadShort.pipe(res);
+// res.end(`<html><body><h1>This is HTML</h1></body></html>`);
+let fileContent = fs.readFile('./comment.html', 'utf8',function(err, data) {
+        if (err) throw err;
+        console.log('OK: ' + filename);
+        console.log(data)
+    });
+console.log("RRRR");
+console.log(fileContent);
+// res.end(fs.readFile(__dirname + '/comment.html', 'utf8'));
+res.end('HHHHHHHHHHHH')
+// myReadShort.pipe(res);
 });
 
-server.listen(3000, '192.168.0.102');
+server.listen(3000, 'localhost');
 console.log("Мв отслеживаем порт 3000");
